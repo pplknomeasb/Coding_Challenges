0b2b8510ce23b188b09511667f2ada5a82ac554e
pplknomeasb <thompson.brennan@gmail.com>
2021-07-01 15:15:05 -0400

    Coding challenge to print inputted string to Camel Case

--- src/org/perscholas/Challenge.java
+++ src/org/perscholas/Challenge.java
@@ -1,40 +1,45 @@
 package org.perscholas;
 
+import java.util.Locale;
+
 public class Challenge {
 
     static String toCamelCase(String s){
 
+        //creating a string builder object to contain the wanted return string
         StringBuilder sb = new StringBuilder();
+
+        //variable will catch when characters are to be capitalized
         boolean isCapital = false;
 
+        //creating loop to loop through the string
         for(int i = 0; i < s.length(); i++){
 
-            if(isCapital=false){
+            //checks if the current character is in a place to be capitalized
+            if(isCapital && Character.isLetter(s.charAt(i))){
 
-                if(Character.isLetter(s.charAt(i))) {
+                //stores the capital letter in the string builder
+                sb.append(s.toUpperCase().charAt(i));
 
-                    sb.append(s.charAt(i));
+                //ensures that the next index will not be capitalized
+                isCapital = false;
 
-                //Checks to see if the next character is a letter
-                    if (Character.isLetter(s.charAt(i + 1)) == isCapital) {
 
-                        //found dilimiter
-                        isCapital = true;
-
-                        //Moves the pointer to index of the foriegn character
-                        i += 1;
-
-                    }
-                 }
-
+            }
+            //checks to see if the character is to be printed
+            else if(Character.isLetter(s.charAt(i)) && !isCapital){
                 sb.append(s.charAt(i));
 
-            }else if(isCapital=true){
-                sb.append(s.toUpperCase().charAt(i));
-                isCapital=false;
             }
+            //checks to see if it is a character separater, next character will be uppercase
+            else if(!Character.isLetter(s.charAt(i))){
+                isCapital=true;
+            }
+
         }
         System.out.println(sb.toString());
+
+        //returns result
         return sb.toString();
     }
 }
--- src/org/perscholas/MainRunner.java
+++ src/org/perscholas/MainRunner.java
@@ -4,7 +4,7 @@ public class MainRunner {
     public static void main(String[] args) {
 
         Challenge challenge = new Challenge();
-        challenge.toCamelCase("the_Stealth_Warrior");
+        challenge.toCamelCase("the)stealth#warrior");
 
         System.out.println("you got this my g");
     }
